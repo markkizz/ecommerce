1a8122e3ae74d928f29981fee6964741214802db
markkizz <mark.pattanapara@gmail.com>
2020-02-12 23:02:45 +0700

    Update MenuItem

--- src/components/MenuItem/MenuItem.js
+++ src/components/MenuItem/MenuItem.js
@@ -1,9 +1,9 @@
 import React from "react";
 import './MenuItem.scss'
 
-export default function MenuItem({title, imageUrl}) {
+export default function MenuItem({title, imageUrl, size}) {
   return (
-    <div className="menu-item" style={{backgroundImage: `url(${imageUrl})`}}>
+    <div className={`${size} menu-item`} style={{backgroundImage: `url(${imageUrl})`}}>
       <div className="content">
         <div className="title">{title}</div>
         <span className="subtitle">SHOP NOW</span>
